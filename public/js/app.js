a731bd1cfdef9018a4e18fe2b5fda575648cb02c
kouta1612 <kouta1612world69@gmail.com>
2019-05-01 18:23:43 +0900

    complete #21

--- public/js/app.js
+++ public/js/app.js
@@ -36,30 +36,38 @@ class Form {
         this.errors = new Errors();
     }
     data() {
-        let data = Object.assign({}, this);
+        let data = {};
 
-        delete data.originalData;
-        delete data.errors;
-
+        for (let property in this.originalData) {
+            data[property] = this[property];
+        }
         return data;
     }
     reset() {
         for (let field in this.originalData) {
             this[field] = '';
         }
+        this.errors.clear();
     }
     submit(requestType, url) {
-        axios[requestType](url, this.data())
-            .then(this.onSuccess.bind(this))
-            .catch(this.onFail.bind(this));
+        return new Promise((resolve, reject) => {
+            axios[requestType](url, this.data())
+                .then(response => {
+                    this.onSuccess(response.data);
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    this.onFail(error.response.data.errors);
+                    reject(error.response.data.errors);
+                });
+        });
     }
-    onSuccess(response) {
-        alert(response.data.message);
-        this.errors.clear();
+    onSuccess(data) {
+        alert(data.message);
         this.reset();
     }
-    onFail(error) {
-        this.errors.record(error.response.data.errors);
+    onFail(errors) {
+        this.errors.record(errors);
     }
 }
 
@@ -75,7 +83,9 @@ new Vue({
 
     methods: {
         onSubmit() {
-            this.form.submit('post', '/projects');
+            this.form.submit('post', '/projects')
+                .then(data => console.log(data))
+                .catch(errors => console.log(errors));
         }
     }
 });
